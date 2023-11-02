ea8add2b7384e232c981d13580377771d6063e26
blasks <blasks@uw.edu>
2023-11-01 18:01:12 -0700

    got docker container running

---
+++ containers/build-containers.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Use these commands to build all the requisite  containers for running the
+# analyses in this manuscript. Standard bioinformatics tool containers are 
+# preferentially pulled from the developer when possible. If no container is 
+# maintained by the developer, the container is pulled from biocontainers
+# (see https://biocontainers.pro/). Two custom containers are also constructed:
+# one with the software necessary for pre-processing fastq files, and another
+# for running all Python code.
+
+#################################################
+# Select Singularity or Docker containerization #
+#################################################
+CONTAINER=""
+while [ "${CONTAINER}" == "" ]; do
+    printf "\nPlease select an option:\n\n\t1 - singularity\n\t2 - docker\n\nEnter '1' or '2': "
+    read SELECTION 
+    if [ "${SELECTION}" == "1" ]; then
+        echo "Continuing with Singularity containerized workflow"
+        CONTAINER="singularity"
+    elif [ "${SELECTION}" == "2" ]; then
+        echo "Continuing with Docker containerized workflow"
+        CONTAINER="docker"
+    else 
+        printf "\nInvalid selection\n"
+    fi
+done
+
+####################
+# Build containers #
+####################
+if [ "${CONTAINER}" == "singularity" ]; then
+    # Pull Singularity containers for tools in which one is maintained by the developer.
+    singularity build salmon.sif docker://combinelab/salmon:1.10.2
+    # # build singularity image from tarball
+    # singularity build fastq-preprocess.sif docker-archive://fastq-preprocess.tar.gz
+elif [ "${CONTAINER}" == "docker" ]; then
+    # Pull Docker containers for tools in which one is maintained by the developer.
+    docker pull combinelab/salmon:1.10.2
+    # export poetry environment requirements
+    if [[ ! -e requirements.txt ]]; then
+        poetry export --without-hashes >> requirements.txt
+    fi
+    # make Docker containers from Dockerfiles
+    docker image build -t barnacle .
+    docker save barnacle | gzip > barnacle.tar.gz
+fi
+
